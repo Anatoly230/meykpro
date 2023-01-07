8665a41b6a5783f262a33ad8e04baaa03a3d339d
Anatoly230 <maximos225@mail.ru>
2023-01-07 03:25:50 +0300

    о нас, почему у нас, форма обратной связи сделаны

---
+++ source/js/toggle-menu.js
@@ -0,0 +1,47 @@
+/* stylelint-disable */
+
+const header = document.querySelector('.header');
+const toggleMenu = header.querySelector('.toggle-menu');
+const buttonsBlock = header.querySelector('.header__buttons-wrapper');
+const menu = header.querySelector('.menu');
+
+
+
+function testMenu() {
+  if (toggleMenu.classList.contains('toggle-menu--hide')) {
+    toggleMenu.classList.remove('toggle-menu--hide')
+    buttonsBlock.style.justifyContent = 'space-between';
+    menu.classList.add('menu--hide');
+  }
+}
+
+testMenu();
+
+function isEscape(e) {
+  return e.key === 'Escape';
+}
+
+function closeMenu(e) {
+  if (isEscape(e)) {
+    getToggle()
+    window.removeEventListener('keydown', closeMenu);
+  }
+}
+function getToggle() {
+  menu.classList.toggle('menu--hide');
+  menu.classList.toggle('menu--show');
+  toggleMenu.classList.toggle('toggle-menu--close');
+}
+
+
+function openMenu(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('toggle-menu')) {
+    getToggle()
+    window.addEventListener('keydown', closeMenu)
+  }
+}
+
+toggleMenu.addEventListener('click', openMenu)
+
+
