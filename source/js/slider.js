6bcb416c773b83cfbe4b48ea64c6360ff7327033
Anatoly230 <maximos225@mail.ru>
2023-02-11 23:23:23 +0300

    слайдер предварительно завершён

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,38 +1,52 @@
+import { defineIndex } from "./utils.js";
 import { getParentElement } from "./rendering.js";
 
 const sliderBlock = document.querySelector('.micflag__slider');
 const slider = sliderBlock.querySelector('.slider');
-const sliders = sliderBlock.querySelectorAll('.slide');
+// const sliders = sliderBlock.querySelectorAll('.slide');
 let sliderCount = slider.children.length;
-let counter = 0;
+// let counter = 0;
 const buttonsList = document.querySelector('.buttons-list');
 const buttonPrevios = buttonsList.children[0],
-buttonNext = buttonsList.children[buttonsList.children.length - 1];
-let slideButtons = document.querySelectorAll('.buttons-list__slide');
+    buttonNext = buttonsList.children[buttonsList.children.length - 1];
+let slideButtons = document.querySelectorAll('.buttons-list__slide'),
+    paginationBlock = setPagination(),
+    pagination;
+
 
 function isCountEqual(count, counter) {
     return count === counter + 1;
 }
 
+function getCustomSlide(index) {
+    slider.style.transform = `translateX(-${(index - 1) * 100}%)`;
+    pagination.counter = index - 1;
+    pagination.assignCurrent(index);
+}
+
 function getNextSlide() {
-    if (isCountEqual(sliderCount, counter)) {
+    if (isCountEqual(sliderCount, pagination.counter)) {
         slider.style.transform = 'translateX(0%)';
-        counter = 0;
+        pagination.counter = 0;
+
     } else {
-        counter++;
-        slider.style.transform = `translateX(-${counter * 100}%)`;
+
+        pagination.counter++;
+        slider.style.transform = `translateX(-${pagination.counter * 100}%)`;
     }
+    pagination.assignCurrent(pagination.counter + 1)
+
 }
 
 function getPreviosSlide() {
-    if (counter === 0) {
+    if (pagination.counter === 0) {
         slider.style.transform = `translateX(-${(sliderCount - 1) * 100}%)`;
-        counter = sliderCount - 1;
+        pagination.counter = sliderCount - 1;
     } else {
-        counter--;
-        slider.style.transform = `translateX(-${counter * 100}%)`;
+        pagination.counter--;
+        slider.style.transform = `translateX(-${pagination.counter * 100}%)`;
     }
-    window.setTimeout(3000)
+    pagination.assignCurrent(pagination.counter + 1)
 }
 
 function getSlideButtons() {
@@ -41,18 +55,18 @@ function getSlideButtons() {
     })
 }
 
-
-if (sliderCount >= 2) {
-    buttonPrevios.after(setPagination());
-    getSlideButtons();
-    slider.style.transitionDuration = '400ms';
-    slider.style.transitionProperty = 'transform';
-    window.setInterval(getNextSlide, 3000)
+function getStartSlider() {
+    if (sliderCount >= 2) {
+        buttonPrevios.after(paginationBlock);
+        pagination = setCurrentPagination();
+        pagination.defineCurrent();
+        getSlideButtons();
+        window.setInterval(getNextSlide, 3000);
+        buttonPrevios.addEventListener('click', getPreviosSlide);
+        buttonNext.addEventListener('click', getNextSlide);
+        paginationBlock.addEventListener('click', onCustomSlide);
+    }
 }
-
-
-
-
 
 
 function setPagination() {
@@ -67,18 +81,46 @@ function setPagination() {
         }
         paginationList.appendChild(paginationItem);
     }
-
     return paginationList;
 }
 
 function setCurrentPagination() {
-    let buttons = document.querySelectorAll('.buttons-list__item')
-    console.log(buttons);
+    const buttons = Array.from(document.querySelector('.buttons-list__wrapper')
+        .querySelectorAll('.buttons-list__item'));
+    let current = null;
+    return {
+        defineCurrent: function () {
+            for (let i = 0; i < buttons.length; i++) {
+                if (buttons[i].classList.contains('buttons-list__item--current')) {
+                    current = buttons[i];
+                    break;
+                }
+            }
+        },
+        getCurrent: function () {
+            return current;
+        },
+        setCurrent: function (index) {
+            current = buttons[index - 1];
+        },
+        assignCurrent: function (index) {
+            current.classList.toggle('buttons-list__item--current');
+            this.setCurrent(index);
+            current.classList.toggle('buttons-list__item--current');
+        },
+        counter: 0,
+    }
 }
 
-buttonPrevios.addEventListener('click', getPreviosSlide);
-buttonNext.addEventListener('click', getNextSlide);
+function onCustomSlide(e) {
+    if (!e.target.classList.contains('buttons-list__item--current') && e.target.classList.contains('buttons-list__item')) {
+        let index = defineIndex(e.target);
+        getCustomSlide(index);
+    }
+}
 
-setCurrentPagination()
+
+getStartSlider()
+
 
 export { sliderBlock };
