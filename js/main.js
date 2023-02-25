62457b2cd5bc6be1958f39deac369a542711c36f
Anatoly <maximos225@mail.ru>
2023-02-25 21:14:07 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-import { toggleMenu } from "./toggle-menu.js";
-import { price } from "./price-revival.js";
-import { sliderBlock } from "./slider.js";
-import { portfolioBlock } from "./portfolio.js";
-// import { test } from "./buble-title.js";
+import{toggleMenu}from"./toggle-menu.js";import{price}from"./price-revival.js";import{sliderBlock}from"./slider.js";import{portfolioBlock}from"./portfolio.js";
