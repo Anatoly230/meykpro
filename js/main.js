c164fcfe49adc2a916236b1e688ba37de9889562
Anatoly <maximos225@mail.ru>
2023-02-28 15:29:35 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-import{toggleMenu}from"./toggle-menu.js";import{price}from"./price-revival.js";import{sliderBlock}from"./slider.js";import{portfolioBlock}from"./portfolio.js";
+import{toggleMenu}from"./toggle-menu.js";import{price}from"./price-revival.js";import{sliderBlock}from"./slider.js";import{portfolioBlock}from"./portfolio.js";import{swipeTest}from"./swiper.js";
