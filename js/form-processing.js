21f45d41586bc7fa928501568d901d2c1288f579
Anatoly230 <maximos225@mail.ru>
2023-03-04 22:58:54 +0300

    Updates

--- js/form-processing.js
+++ js/form-processing.js
@@ -1,6 +1,8 @@
 
 
 const form = document.querySelector('.contacts__form');
+const submitBtn = form.querySelector('.form__submit');
+
 const config = {
     classTo: 'input',
     errorClass: 'input--error',
@@ -38,14 +40,24 @@ function validateNickname(value) {
     return value.length >= 2 && value.length <= 50;
 }
 
+function onSubmit(e) {
+    let isValid = pristine.validate();
+    if (isValid) {
+        submitBtn.disbled = true
+    } else {
+        e.preventDefault()
+        submitBtn.disbled = false
+    }
+
+}
+
 console.log(form)
 console.log(some)
 
 pristine.addValidator(some, validateNickname, 'От 2 до 50 символов')
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    pristine.validate()
-})
+
+
+form.addEventListener('submit', onSubmit)
 
 export { form };
